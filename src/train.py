46e55dea4ee3da3429d5a0c9bcf7a61e782d493f
kevinleewy <kevinleewy@hotmail.com>
2022-02-04 15:49:25 -0800

    Added accuracy calculations and setting rng seed

--- src/train.py
+++ src/train.py
@@ -1,3 +1,6 @@
+import numpy as np
+import os
+import random
 import torch
 from torch.utils.data import Dataset, DataLoader, Subset
 from torch import nn
@@ -6,14 +9,24 @@ from tqdm import tqdm
 from model import ResnetDummy
 from dataloader import load_datasets
 
-BASE_PATH = '/Users/kevinlee/Data/Stanford/CS329S/project/CS329S-project/data/In-shop Clothes Retrieval Benchmark'
-# BASE_PATH = '/scratch/users/avento/deepfashion'
+# BASE_PATH = '/Users/kevinlee/Data/Stanford/CS329S/project/CS329S-project/data/In-shop Clothes Retrieval Benchmark'
+BASE_PATH = '/scratch/users/avento/deepfashion'
 LEARNING_RATE = 0.01
 EPOCHS = 50
 BATCH_SIZE = 64
+WEIGHTS_SAVE_FREQUENCY = 10
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+RNG_SEED = 17
+
+def set_seed(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
 
 def train():
+
+    set_seed()
 
     datasets, num_labels = load_datasets(base_path=BASE_PATH)
 
@@ -33,6 +46,8 @@ def train():
 
     criterion = nn.BCEWithLogitsLoss(reduction='mean') # TODO: Confirm this works when doing vector to vector within each sample
 
+    best_accuracy = 0.0
+
     for epoch in range(EPOCHS):
         model.train()
         for imgs, attributes in tqdm(train_dataloader):
@@ -48,16 +63,38 @@ def train():
 
         model.eval()
         with torch.no_grad():
+            correct = 0.
+            total = 0.
             for imgs, attributes in tqdm(eval_dataloader):
                 imgs, attributes = imgs.to(DEVICE).float(), attributes.to(DEVICE).float()
                 out = model(imgs)
 
-            # TODO: Calculate accuracy/F1
+                outputs = torch.sigmoid(outputs)
+                predictions = torch.round(outputs)
+                total += attributes.size(0)
+                correct += (predictions == attributes).sum().cpu().item()
+            accuracy = 100 * correct / total
+            print("Accuracy: {}%".format(accuracy))
 
-            # TODO: Save if best accuracy/F1
+            if accuracy > best_accuracy:
+                best_accuracy = accuracy
+                torch.save(model.state_dict(), os.path.join(BASE_PATH, "best_model.pt"))
 
-        # TODO: Save every N epochs
-
+        if epoch % WEIGHTS_SAVE_FREQUENCY == 0:
+            torch.save({
+                "epoch": epoch,
+                "best_accuracy": best_accuracy,
+                "model": model.state_dict(),
+                "optimizer": optimizer.state_dict(),
+            }, os.path.join(BASE_PATH, f"checkpoint_{epoch}.pt"))
+    
+    # Save one last checkpoint
+    torch.save({
+        "epoch": epoch,
+        "best_accuracy": best_accuracy,
+        "model": model.state_dict(),
+        "optimizer": optimizer.state_dict(),
+    }, os.path.join(BASE_PATH, f"checkpoint_{epoch}.pt"))
 
 
     # TODO: Add query/gallery split
