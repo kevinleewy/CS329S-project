f9438a9655d7d86401a8ce18e165c6ffe9b687f2
Anthony Vento <anthony.vento@utexas.edu>
2022-03-03 04:48:55 -0800

    modified embedding script

--- src/models/gen_embeddings.py
+++ src/models/gen_embeddings.py
@@ -12,40 +12,95 @@ from model import ResnetDummy
 from dataloader import load_datasets, get_data_transforms
 
 BASE_PATH = '/scratch/users/avento/deepfashion'
-WEIGHTS_PATH = os.path.join(BASE_PATH, '2022-02-08_23-53-14/best_model.pt')
-CATALOG_PATH = os.path.join(BASE_PATH, 'catalog.json')
+SAVE_PATH = os.path.join(BASE_PATH, 'embeddings')
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def generate_save_catalog_embeddings():
-    datasets, attr_counts = load_datasets(base_path=BASE_PATH)
-    num_labels = attr_counts.shape[0]
+model_metadata = {
+    "model_13": {
+        "weights": os.path.join(BASE_PATH, '2022-02-08_23-53-14/best_model.pt'),
+        "model_args":{'num_labels':13, 'embedding_dim':512, 'freeze_pretrain':False},
+    },
+    "model_36": {
+        "weights": os.path.join(BASE_PATH, '2022-02-28_07-05-40/best_model.pt'),
+        "model_args":{'num_labels':36,'embedding_dim':512, 'freeze_pretrain':False},
+    },
+    "model_36_aug":  {
+        "weights": os.path.join(BASE_PATH, '2022-02-28_16-52-37/best_model.pt'),
+        "model_args": {'num_labels':36,'embedding_dim':512, 'freeze_pretrain':False},
+    },
+}
 
-    model = ResnetDummy(num_labels, freeze_pretrain=False)
-    model.load_state_dict(torch.load(WEIGHTS_PATH))
+catalog_metadata = {
+    "gallery": {
+        "catalog_dir": os.path.join(BASE_PATH, 'catalog.json'),
+    },
+    "gap": {
+        "img_dir":  os.path.join(BASE_PATH, "webscraped_images", "gap")
+    },
+    "express": {
+        "img_dir":  os.path.join(BASE_PATH, "webscraped_images", "express")
+    },
+    "zappos": {
+        "img_dir":  os.path.join(BASE_PATH, "webscraped_images", "zappos")
+    }
+}
+
+def get_model(model_name):
+    weights_path = model_metadata[model_name]["weights"]
+    model_args = model_metadata[model_name]["model_args"]
+    model = ResnetDummy(**model_args)
+    model.load_state_dict(torch.load(weights_path))
     model = model.to(DEVICE)
+    return model
+
+
+def generate_save_catalog_embeddings(model_name, catalog_name):
+    model = get_model(model_name)
     model.eval()
-
-    with open(CATALOG_PATH) as f:
-        catalog = json.load(f)
-
-    all_embeds = []
     transforms = get_data_transforms()['test']
-    with torch.no_grad():
-        for item_dict in tqdm(catalog):
-            uri = item_dict['URI']
-            full_img_path = os.path.join(BASE_PATH, uri)
+    all_embeds = []
 
-            img = Image.open(full_img_path)
-            img = transforms(img).unsqueeze(0).to(DEVICE)
+    img_dir = catalog_metadata[catalog_name].get("img_dir")
+    if not img_dir:
+        catalog_dir = catalog_metadata[catalog_name]["catalog_dir"]
+        with open(catalog_dir) as f:
+            catalog = json.load(f)
 
-            embed = model.embed(img)
-            all_embeds.append(embed.cpu())
+        with torch.no_grad():
+            for item_dict in tqdm(catalog):
+                uri = item_dict['URI']
+                full_img_path = os.path.join(BASE_PATH, uri)
+
+                img = Image.open(full_img_path)
+                img = transforms(img).unsqueeze(0).to(DEVICE)
+
+                embed = model.embed(img)
+                all_embeds.append(embed.cpu())
+    else:
+        files = os.listdir(img_dir)
+        with torch.no_grad():
+            for index in tqdm(range(len(files))):
+                full_img_path = os.path.join(img_dir, f"{index}.jpg")
+
+                img = Image.open(full_img_path)
+                img = transforms(img).unsqueeze(0).to(DEVICE)
+
+                embed = model.embed(img)
+                all_embeds.append(embed.cpu())
+
+         
 
     all_embeds = np.concatenate(all_embeds, axis=0)
-    np.save(os.path.join(BASE_PATH, 'catalog_embeds.npy'), all_embeds)
+
+    cur_save_path = os.path.join(SAVE_PATH, model_name)
+    os.makedirs(cur_save_path, exist_ok=True)
+    np.save(os.path.join(cur_save_path, f'{catalog_name}_embeds.npy'), all_embeds)
 
 if __name__ == '__main__':
-    generate_save_catalog_embeddings()
+    for model in model_metadata.keys():
+        for catalog in catalog_metadata.keys():
+            print(f"Running for {model} and {catalog}...")
+            generate_save_catalog_embeddings(model, catalog)
 
 
 
