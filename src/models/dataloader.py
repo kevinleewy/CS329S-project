eefa0c31994f162916e64b31b80d886580abb585
kevinleewy <kevinleewy@hotmail.com>
2022-03-04 01:18:22 -0800

    U-Net

--- src/models/dataloader.py
+++ src/models/dataloader.py
@@ -18,8 +18,8 @@ default_data_transforms = {
     'train': transforms.Compose([
         transforms.RandomResizedCrop(CROP_SIZE),
         transforms.RandomHorizontalFlip(),
-        # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
-        # transforms.RandomRotation(degrees=10),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+        transforms.RandomRotation(degrees=10),
         transforms.ToTensor(),
         transforms.Normalize(NORM_MEAN, NORM_STD)
     ]),
@@ -37,22 +37,53 @@ default_data_transforms = {
     ]),
 }
 
+unet_combined_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(CROP_SIZE),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(degrees=10),
+    ]),
+    'validation': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(CROP_SIZE),
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(CROP_SIZE),
+    ]),
+}
+
+unet_image_transforms = {
+    'train': transforms.Compose([
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+        transforms.Normalize(0, 1)
+    ]),
+    'validation': transforms.Compose([
+        transforms.Normalize(0, 1)
+    ]),
+    'test': transforms.Compose([
+        transforms.Normalize(0, 1)
+    ]),
+}
+
 def get_data_transforms():
     return default_data_transforms
 
 def load_datasets(
     base_path:str='/',
     splits: List = [0.8, 0.1, 0.1],
-    data_transforms: Dict[str, Any]=default_data_transforms
+    num_labels: int = 36,
+    data_transforms: Dict[str, Any]=default_data_transforms,
+    segment: bool = False,
 ):
     
-    dataset_attrs_path = os.path.join(base_path, 'train_dataset_attr_13.npy')
+    dataset_attrs_path = os.path.join(base_path, f'train_dataset_attr_{num_labels}.npy')
     dataset_attrs = np.load(dataset_attrs_path, allow_pickle=True)
 
     dataset_images_path = os.path.join(base_path, 'train_dataset_images.npy')
     dataset_images = np.load(dataset_images_path, allow_pickle=True)
 
-    dataset_attr_counts_path = os.path.join(base_path, 'train_dataset_attr_13_counts.npy')
+    dataset_attr_counts_path = os.path.join(base_path, f'train_dataset_attr_{num_labels}_counts.npy')
     dataset_attr_counts = np.load(dataset_attr_counts_path, allow_pickle=True)
 
     n, _ = dataset_images.shape
@@ -74,7 +105,14 @@ def load_datasets(
     datasets = {}
     for i, (split, transform) in enumerate(data_transforms.items()):
 
-        datasets[split] = DeepFashion(dataset_splits[i], dataset_attrs, base_path, transform=transform)
+        if segment:
+            datasets[split] = DeepFashionWithSegmentation(dataset_splits[i], dataset_attrs, base_path,
+                                        image_transform=unet_image_transforms[split],
+                                        combined_transform=unet_combined_transforms[split])
+        else:
+            datasets[split] = DeepFashion(dataset_splits[i],
+                                        dataset_attrs, base_path,
+                                        transform=transform)
 
     return datasets, dataset_attr_counts
 
@@ -94,6 +132,7 @@ class DeepFashion(Dataset):
         img_path = self.dataset[idx][1]
 
         full_img_path = os.path.join(self.base_path, img_path)
+
         x = Image.open(full_img_path)
         
         y = self.attrs[item_id]
@@ -101,4 +140,72 @@ class DeepFashion(Dataset):
         if self.transform:
             x = self.transform(x)
 
-        return x, y
+        return x, y, None, None
+
+class DeepFashionWithSegmentation(Dataset):
+    def __init__(self, dataset=[], attrs={}, base_path:str='/',
+        image_transform=None,
+        combined_transform=None
+    ):
+        self.dataset = dataset
+        self.attrs = attrs
+        self.base_path = base_path
+        self.base_path += '/Img'
+        self.image_transform = image_transform
+        self.combined_transform = combined_transform
+        
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+
+        item_id = self.dataset[idx][0]
+        img_path = self.dataset[idx][1]
+
+        # Use highres image for segmentation
+        full_img_path = os.path.join(self.base_path, img_path).replace('img/', 'img_highres/')
+
+        seg_im_path = full_img_path.replace('.jpg', '_segment.png')
+
+        x = Image.open(full_img_path)
+        x = transforms.ToTensor()(x)
+        seg, hasSeg = loadSegmentationArray(seg_im_path, x.shape)
+        seg = transforms.ToTensor()(seg)
+
+        if self.image_transform:
+            x = self.image_transform(x)
+
+        # Combine image and seg data for transformations
+        x = torch.cat((x, seg))
+
+        if self.combined_transform:
+            x = self.combined_transform(x)
+
+        y = self.attrs[item_id]
+
+        # Split image and seg data
+        x, seg = x[:3, :, :], x[3:, :, :]
+        
+        return x, y, seg, hasSeg
+
+# isEqualFunc is a function that maps each element x in a numpy
+# array to 1 if x does not equal a value v, and 0 otherwise.
+isNotEqualFunc = np.vectorize(lambda x, v: 1 if x != v else 0)
+
+def loadSegmentationArray(seg_im_path, shape):
+
+    if os.path.exists(seg_im_path):
+        im_frame = Image.open(seg_im_path)
+
+        # Convert to numpy array and discard alpha channel (channel 4)
+        np_frame = np.array(im_frame)[:,:,:3]
+
+        # Create a segmentation heatmap where each pixel is 1 for foreground
+        # (RGB sum is non-zero), and 0 for background (RGB sum is zero)
+        seg = isNotEqualFunc(np_frame.sum(axis=2), 0)[:,:,np.newaxis]
+        
+        return seg, 1
+    else:
+        seg = np.zeros((shape[1], shape[2], 1))
+        return seg, 0
+    
--- src/models/metrics.py
+++ src/models/metrics.py
@@ -21,3 +21,16 @@ def iou(y_true: torch.Tensor, y_pred: torch.Tensor) -> float:
 
     # Iterate over true classes so we never have a zero division
     return [_class_iou(c).item() for c in true_classes if c != 0]
+
+
+def dice_coefficient(outputs, targets, threshold=0.5, eps=1e-8):
+    batch_size = targets.size(0)
+    y_pred = outputs[:,0,:,:,:]
+    y_truth = targets[:,0,:,:,:]
+    y_pred = y_pred > threshold
+    y_pred = y_pred.type(torch.FloatTensor)
+    intersection = torch.sum(torch.mul(y_pred, y_truth)) + eps/2
+    union = torch.sum(y_pred) + torch.sum(y_truth) + eps
+    dice = 2 * intersection / union 
+    
+    return dice / batch_size
