126686f45fca10f00873a59164db2be562b308ea
Manan Rai <mananrai@stanford.edu>
2022-03-06 13:41:35 -0800

    minor utils fix for put request

--- src/backend/fashflix/common/django_utils.py
+++ src/backend/fashflix/common/django_utils.py
@@ -2,6 +2,8 @@ def get_param(request, param, default=None):
     try:
         if request.method == "POST":
             return request.data[param]
+        if request.method == "PUT":
+            return request.body[param]
         elif request.method == "GET":
             return request.query_params[param]
         else:
