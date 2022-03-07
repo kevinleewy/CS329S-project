63e2f0f2a85d4a051a79391e255a9868b56565bd
Manan Rai <mananrai@stanford.edu>
2022-03-06 18:18:21 -0800

    more frontend cleanup

--- src/webapp/frontend/src/pages/LandingPage/LandingPage.js
+++ src/webapp/frontend/src/pages/LandingPage/LandingPage.js
@@ -1,48 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react';
-import axios from "axios";
+import React from 'react';
 
-import CircleType from "circletype";
-
-import { Layout, Menu, Breadcrumb } from 'antd';
-import Icon, {
-  AppstoreOutlined,
-  DesktopOutlined,
-  FileOutlined,
-  FireOutlined,
-  HomeOutlined,
-  PieChartOutlined,
-  SkinOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
 import styled from "styled-components";
 
 import "antd/dist/antd.css"
 import './styles.css';
 
-import SwipableCards from '../../components/SwipableCards/SwipableCards';
-
-import CatalogPage from '../CatalogPage/CatalogPage';
-import SearchPage from '../SearchPage/SearchPage';
-import PersonalizationPage from '../PersonalizationPage/PersonalizationPage';
-
-import { AUTHENTICATE_TOKEN, GET_RECOMMENDATIONS, GUEST_ACCOUNT } from '../../apiPaths';
-
-
-const { SubMenu } = Menu;
-const { Header, Content, Footer, Sider } = Layout;
-
-const Title = styled.div`
-  font-size: 24px;
-  color: black;
-  text-align: left;
-`;
-
-const StyledSpan = styled.span`
-  & div {
-    position: none !important;
-  }
-`;
 
 const Container = styled.div`
   & {
@@ -56,30 +18,6 @@ const Container = styled.div`
     align-items:center;
   }
 `
-
-const StyledSwipableCards = styled(SwipableCards)`
-  background: red; // #f9fafb;
-  // width: 300px;
-  // height: 400px;
-  // display: flex;
-  // align-items: center;
-  // justify-content: center;
-  // flex-direction: column;
-  // font-size: 150px;
-  text-shadow: 0 10px 10px #d1d5db;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
-  border-radius: 50px;
-  border: red;
-  background-color: red !important;
-`
-
-function LogoIcon() {
-  return (
-    <span style={{fontSize: "40px"}}>
-      🧥
-    </span>
-  )
-}
 
 const ACCEPTED_STATUS_CODE = 200;
 
