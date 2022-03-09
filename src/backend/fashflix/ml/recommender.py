01a57a9a334efea49271c3dfc0fa8a4349a8402d
Manan Rai <mananrai@stanford.edu>
2022-03-08 21:49:28 -0800

    return different recommendations from different endpoints

--- src/backend/fashflix/ml/recommender.py
+++ src/backend/fashflix/ml/recommender.py
@@ -17,22 +17,53 @@ from common.config import Config as PipelineConfig
 
 
 class Config:
-	NUM_RECOMMENDATIONS = 2
+	NUM_RECOMMENDATIONS = 200
 
 
 class KNearestRecommender:
 
 	def __init__(self, n_neighbors=Config.NUM_RECOMMENDATIONS):
 		self.n_neighbors = n_neighbors
-		self.recommender = NearestNeighbors(n_neighbors=n_neighbors)
+		self.recommender = NearestNeighbors(n_neighbors=2*n_neighbors)
 
 
 	def fit(self, embeddings):
 		self.recommender.fit(embeddings)
 
 
-	def get_recommendations(self, query_embedding, catalog_df):
+	def get_recommendations(self, query_embedding, catalog_df, eps=1e-8):
 		distances, indices = self.recommender.kneighbors(query_embedding)
+		final_indices, prev_distance = [], None
+		for distance, index in zip(distances[0], indices[0]):
+			if abs(distance - prev_distance) >= eps: # To ensure no catalog dupes
+				prev_distance = distance
+				final_indices.append(index)
+				if len(final_indices) == self.n_neighbors:
+					break
+		return catalog_df.uuid.values[final_indices]
+
+
+class KNearestRecommender2:
+
+	def __init__(self, n_neighbors=Config.NUM_RECOMMENDATIONS):
+		self.n_neighbors = n_neighbors
+
+
+	def fit(self, embeddings):
+		self.embeddings = embeddings
+
+
+	def get_recommendations(self, query_embedding, catalog_df, eps=1e-8):
+		distances = np.sum((self.embeddings - query_embedding.flatten()) ** 2, axis=0)
+		distances[distances <= eps] = float("inf")
+		sort_idxs, indices, prev_dist = np.argsort(distances), [], None
+		for sort_idx in sort_idxs:
+			curr_dist = distances[sort_idx]
+			if curr_dist != prev_dist: # To ensure no catalog dupes
+				prev_dist = curr_dist
+				indices.append(sort_idx)
+				if len(indices) == self.n_neighbors:
+					break
 		return catalog_df.uuid.values[indices]
 
 
@@ -59,3 +90,6 @@ class WeightedPreferenceOptimizer:
 			coeff = 1 if vote else -1
 			preference_vector = self.one_min_alpha * preference_vector + self.alpha * np.array(embedding)
 		return preference_vector.tolist()
+
+
+# class SupervisedRecommender:
